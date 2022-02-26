2a96d84440ec84528ffd16c32b40718b684f52c9
Jared Jackson <joj_lan@yahoo.com>
2022-02-25 23:50:23 -0700

    Added the ability to gradiant color the plot

--- a2_chaos/chaos_small.m
+++ a2_chaos/chaos_small.m
@@ -2,14 +2,12 @@
 %%% Jared Jackson
 %%% Due Feb 13th
 
-%%% This code is written to model and plot the outcomes of a dripping faucet.
+%%% This code is written to model and plot the outcomes of a dripping faucet./s
 %%%
 %%% The Logistic mapping (not fuction since each input can have multipe
 %%% outputs), is just about the simplest logistic function I think exists.
 %%% Despite this simplicity, it applies well to a variety of situations like
 %%% simple populations, radioactive decay, and of course, dripping water.
-%%%
-%%% 
 
 %%% Just MATLAB Things %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all; clear variables; clc;
@@ -27,7 +25,7 @@ iterations = 1000;
 xRes = 1e4;
 
 %%% NUmber of points in the Y axis; Larger = more resolution
-yRes = 256;
+yRes = 64;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -49,9 +47,9 @@ for i = 2:1:(iterations  + yRes)
 end
 
 %%% Call It UI %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-line(r, X);
-xlim([3.5 4]);
-ylim([0 1]);
+line(r, X, 'Color', 'blue');
+%%lineXColor(r, X, 'hsv');
+set(gca, 'XLim', [0 4], 'YLim', [0 1]);
 title(['The Bifurcation Diagram - ' num2str(xRes) ' r Values']);
 xlabel("Growth rate (r)");
 ylabel("Final value/s");
@@ -59,3 +57,15 @@ ylabel("Final value/s");
 
 %%% See What's Become Of Me
 toc
+
+%%% This will plot the bifurcation diagram with a gradiant color on the x axis
+%%% but it is VERY slow since MATLAB doesn't allow multiple colors per line, so
+%%% it has to plot a new line between each point.
+function lineXColor(x, y, cmap)
+numPoints = length(x);
+pointColor = max(1, round( (0:numPoints) ./ numPoints .* 256));
+cmp = colormap(cmap);
+for i = 1:(numPoints - 1)
+	line([x(i) x(i + 1)], [y(:, i) y(:, i + 1)], "color", cmp(pointColor(i),:) )
+end
+end
