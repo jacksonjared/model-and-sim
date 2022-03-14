bf5e7c8e28fdc2218da46bd96fb84aa60964c786
Jared Jackson <joj_lan@yahoo.com>
2022-03-14 17:04:54 -0600

    Turned in state

--- a3_filterAndNoisr/FnN.m
+++ a3_filterAndNoisr/FnN.m
@@ -12,10 +12,20 @@ Fs = 1e4;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%% Filter parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% Filter center frequency
+Fc = 100;
+
+%%% Filter width
+Fw = 1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%% Signal parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% The stop time of the signal
-stop = 3;
+stop = 4;
 
 %%% signal frequency in hertz
 freq = 100;
@@ -23,8 +33,8 @@ freq = 100;
 %%% signal amplitude
 Samp = 1;
 
-%%% Noise amplitude as a multiple of signal amplitude
-Namp = 8;
+%%% Noise amplitude
+Namp = 1;
 
 %%% Include random noise when corrupting signal?
 includeNoise = 1;
@@ -38,7 +48,7 @@ includeHarmonics = 1;
 Ts = 1 / Fs;
 
 %%% Time of the signal
-time = 0:Ts:(stop - Ts);
+time = 0:Ts:stop;
 
 %%% The signal
 signal = Samp .* sin(2 .* pi .* freq .* time);
@@ -47,8 +57,8 @@ signal = Samp .* sin(2 .* pi .* freq .* time);
 noise = Namp * randn(size(time));
 
 %%% The 2nd, and 3rd harmonic of the signal
-harmonics = (Samp / 1.5) .* sin(2 .* pi .* 2 .* freq .* time) + (Samp / 3) .* ...
-						sin(2 .* pi .* 3 .* freq .* time);
+harmonics = (Samp / 1.5) .* sin(4 .* pi .* freq .* time) + (Samp / 2.25) .* ...
+						sin(6 .* pi .* freq .* time);
 
 %%% Intentionally corrupt the signal with all the unwanted distortions
 corrupted = signal;
@@ -60,79 +70,107 @@ if (includeHarmonics == 1)
 end
 
 %%% Applies the filter and saves the result
-filtered = bandPass2nd(corrupted, freq, 1, Ts);
+filtered = bandPass2nd(corrupted, Fc, Fw, Ts);
 
 %%% Time vs Amplitude
 subplot(3, 2, [1 2]);
 yyaxis left;
 plot(time, filtered, '-b', 'LineWidth', 1);
+ylim([(max(filtered) * -1.1) (max(filtered) * 1.1)]);
 ylabel("Filtered Amplitude");
 yyaxis right;
 plot(time, corrupted, '-r', 'LineWidth', 1);
+ylim([(max(corrupted) * -1.0) (max(corrupted) * 1.0)]);
 ylabel("Noisy Amplitude");
 xlabel("Time [s]");
 grid minor;
-title('Heavily Corrupted Signal and Filtered Signal', 'FontSize', 18);
+title('Noisy and Filtered Time Domain', 'FontSize', 18);
 legend('Filtered', 'Noisy');
 set(gca, 'XLim', [( stop - (3 / freq) ) stop]);
 
 %%% Frequency vs Amplitude of corrupted signal
 subplot(3, 2, 3);
-fftPlot(corrupted, Fs);
-xlabel("Frequency [Hz]");
-ylabel("Frequency Amplitude");
-title('Noisy Frequency Domain', 'FontSize', 18);
-grid minor;
-set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])]);
+s1 = fftPlot(corrupted, Fs, freq);
+title(['Noisy Frequency Domain. The SNR is ' num2str(s1) ' dB'], ...
+	 		 'FontSize', 18);
 
 %%% power spectral density of corrupted signal
 subplot(3, 2, 5);
-psdPlot(corrupted, Fs);
-xlabel("Frequency [Hz]");
-ylabel("Power? Amplitude");
+psdPlot(corrupted, Fs, freq);
 title('Noisy PSD', 'FontSize', 18);
-grid minor;
-set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])]);
 
 %%% Frequency vs Amplitude of filtered signal
 subplot(3, 2, 4);
-fftPlot(filtered, Fs);
-xlabel("Frequency [Hz]");
-ylabel("Frequency Amplitude");
-title('Filtered Frequency Domain', 'FontSize', 18);
-grid minor;
-set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])]);
+s3 = fftPlot(filtered, Fs, freq);
+title(['Filtered Frequency Domain. The SNR is ' num2str(s3) ' dB'], ...
+			 'FontSize', 18);
 
 %%% power spectral density of filtered signal
 subplot(3, 2, 6);
-psdPlot(filtered, Fs);
-xlabel("Frequency [Hz]");
-ylabel("Power? Amplitude");
+psdPlot(filtered, Fs, freq);
 title('Filtered PSD', 'FontSize', 18);
-grid minor;
-set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])]);
 
 %%% See What's Become Of Me
 toc;
 
 %%% Function Junction, What's your Conjunction %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% Plots a power spectral density of a signal. I am very unconfident in this, I
-%%% haven't found enough good resources to understand how this works.
-function psdPlot(signal, Fs)
-	len = length(signal);
-	psd = (abs( fft(signal, len) ) .^2) ./ len ./ Fs;
-	x = Fs .* (0:len / 2) ./ len;
-	plot( x, psd( 1:(len / 2) + 1 ), 'LineWidth', 1 );
+%%% Plots a power spectral density of a signal. I just read wiki articles and
+%%% tried things out until this seemed to work so I am not totally confident in
+%%% it
+function psdPlot(signal, Fs, freq)
+	
+	%%% The length of the input signal
+	len = length(signal) - 1;
+
+	%%% 
+	psd = abs(fft(signal)).^2 ./ (1e4 * len);
+
+	%%% Convert to dBw for better dynamic range
+	psd_dBw = 10 .* log10( psd(1:(len / 2)) );
+
+	%%% Setting and scaling the frequency interval to plot against
+	f = Fs .* (0:(len / 2) - 1) ./ len;
+
+	%%% Plot the PSD in dBw
+	plot( f, psd_dBw, 'LineWidth', 1 );
+
+	%%% Formatting
+	xlabel("Frequency [Hz]");
+	ylabel("Amplitude [dBw]");
+	grid minor;
+	set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])], ...
+					 'YLim', [(-5 + mean(psd_dBw)) (5 + max(psd_dBw))]);
 end
 
 %%% Plots the frequency domain of a signal
-function fftPlot(signal, Fs)
-	y = fft(signal);
-	len = length(signal);
-	yfft = 2 * abs(y / len);
-	xfft = Fs .* ( 0:(len / 2) ) ./ len;
-	plot( xfft, yfft( 1:(len / 2) + 1 ), 'LineWidth', 1 );
+function SNR = fftPlot(signal, Fs, freq)
+
+	%%% The length of the input signal
+	len = length(signal) - 1;
+
+	%%% The amplitude of each frequency that makes up the input signal, aka the
+	%%% fourier transform
+	yfft = 2 .* abs( fft(signal) ./ len );
+	yfft = yfft( 1:(len / 2) );
+
+	yfft_dB = 10 * log10(yfft);
+
+	%%% Setting and scaling the frequency interval to plot against
+	f = Fs .* ( 0:(len / 2) -1 ) ./ len;
+
+	%%% Calculate the signal to noise ratio
+	SNR = round( ( yfft_dB(len / Fs * freq + 1) - mean(yfft_dB) ), 1);
+
+	%%% Plot the fourier transform
+	plot( f, yfft, 'LineWidth', 1 );
+
+	%%% Formatting
+	xlabel("Frequency [Hz]");
+	ylabel("Amplitude");
+	grid minor;
+	set(gca, 'XLim', [0 min([(5 * freq) (Fs / 2)])], ...
+					 'YLim', [0 (1.1 * max(yfft))]);
 end
 
 %%% Applies a 2nd order bandpass filter with a center frequency of f0 and a
